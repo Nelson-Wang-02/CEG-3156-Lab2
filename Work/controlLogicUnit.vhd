591931e9c37976dff897477dffd6fd5f44e30464
Nelson Wang <nelsonwangmain@outlook.com>
2023-06-27 17:08:07 -0400

    Add Jump and BNE control signals.

--- Work/controlLogicUnit.vhd
+++ Work/controlLogicUnit.vhd
@@ -9,15 +9,16 @@ entity controlLogicUnit is
 		MemtoReg, RegWrite : OUT STD_LOGIC;
 		MemRead, MemWrite : OUT STD_LOGIC;
 		Branch : OUT STD_LOGIC;
+		Jump : OUT STD_LOGIC;
 		ALUOp1, ALUOp0 : OUT STD_LOGIC);
 
 end controlLogicUnit; 
 	
 architecture rtl of controlLogicUnit is 
 
-	signal rFormat, lw, sw, beq : STD_LOGIC;
+	signal rFormat, lw, sw, beq, bne, j : STD_LOGIC;
 	
-	signal o_ALUSrc, o_RegWrite : STD_LOGIC;
+	signal o_ALUSrc, o_RegWrite, o_Branch : STD_LOGIC;
 	
 	begin
 	
@@ -26,9 +27,13 @@ architecture rtl of controlLogicUnit is
 	lw <= op(5) and not(op(4)) and not(op(3)) and not(op(2)) and op(1) and op(0);
 	sw <= op(5) and not(op(4)) and op(3) and not(op(2)) and op(1) and op(0);
 	beq <= not(op(5)) and not(op(4)) and not(op(3)) and op(2) and not(op(1)) and not(op(0));
+	bne <= not(op(5)) and not(op(4)) and not(op(3)) and op(2) and not(op(1)) and op(0); 
+	j <= not(op(5)) and not(op(4)) and not (op(3)) and not(op(2)) and op(1) and not(op(0));
+	
 	
 	o_ALUSrc <= lw or sw; 
 	o_RegWrite <= rFormat or lw;
+	o_Branch <= beq or bne; 
 	
 	--output drivers
 	RegDst <= rFormat;
@@ -37,7 +42,8 @@ architecture rtl of controlLogicUnit is
 	RegWrite <= o_RegWrite;
 	MemRead <= lw; 
 	MemWrite <= sw; 
-	Branch <= beq; 
+	Branch <= o_Branch; 
+	Jump <= j;
 	ALUOp1 <= rFormat; 
 	ALUOp0 <= beq; 
 	
